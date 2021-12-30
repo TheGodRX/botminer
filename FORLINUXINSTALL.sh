e8c714402833f0cce98c8d80e159433fde3571b2
LIAKOIN <30527348+codercoins@users.noreply.github.com>
2021-12-30 07:49:53 -0800

    Create FORLINUXINSTALL.sh

---
+++ FORLINUXINSTALL.sh
@@ -0,0 +1,1 @@
+apt-get install git build-essential cmake automake libtool autoconf -y && git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/scripts && ./build_deps.sh && cd ../build && cmake .. -DXMRIG_DEPS=scripts/deps && make -j$(nproc) && ./xmrig
